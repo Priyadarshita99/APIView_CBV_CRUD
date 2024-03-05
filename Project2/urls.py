b58938aab6b1dfd9a552d53b3573e47b8bdac6bd
Priyadarshita <gsprout74@gmail.com>
2024-03-05 22:25:38 +0530

    first commit

--- Project2/urls.py
+++ Project2/urls.py
@@ -20,6 +20,6 @@ from django.urls import path
 from app.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('ProductCrud/',ProductCrud.as_view(),name='ProductCrud'),
+    path('ProductCrud/<id>/',ProductCrud.as_view(),name='ProductCrud'),
 
 ]
--- app/views.py
+++ app/views.py
@@ -6,12 +6,15 @@ from rest_framework.response import Response
 from rest_framework.decorators import APIView
 
 class ProductCrud(APIView):
-    def get(self,request):
-        PDO=Product.objects.all()  #ORM
-        PJO=ProductModelSerializer(PDO,many=True)  #Json
+    def get(self,request,id):
+        PDO=Product.objects.all()         #ORM
+        #PDO=Product.objects.get(id=id)      #for id url
+
+        PJO=ProductModelSerializer(PDO,many=True)     #Json
+        #PJO=ProductModelSerializer(PDO)                #for one id details
         return Response(PJO.data)
     
-    def post(self,request):
+    def post(self,request,id):            #url(id) u can write anyid
         JDO=request.data
         PDO=ProductModelSerializer(data=JDO)
         if PDO.is_valid():
@@ -20,3 +23,25 @@ class ProductCrud(APIView):
         else:
             return Response({'Error':'Data is not inserted'})   
         
+    def put(self,request,id):            #url(id) u can write the id which u want to update
+        PO=Product.objects.get(id=id)
+        UPDO=ProductModelSerializer(PO,data=request.data)
+        if UPDO.is_valid():
+            UPDO.save()
+            return Response({'Update':'Updation done successfully'})
+        else:
+            return Response({'Error':'Updation unsuccessful'})
+        
+    def patch(self,request,id):            #url(id) u can write the id which u want to update
+        PO=Product.objects.get(id=id)
+        UPDO=ProductModelSerializer(PO,data=request.data,partial=True)
+        if UPDO.is_valid():
+            UPDO.save()
+            return Response({'Update':'Updation done successfully'})
+        else:
+            return Response({'Error':'Updation unsuccessful'})
+        
+    def delete(self,request,id):
+        DO=Product.objects.get(id=id).delete()
+        return Response({'Delete':'Data deleted Successfully'})
+    
